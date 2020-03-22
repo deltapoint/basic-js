892b651f0b5d2658abb0b432b5e3cac372f22f15
deltapoint <60572499+deltapoint@users.noreply.github.com>
2020-03-22 23:03:46 +0300

    feat: implement 'dream-team' task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,10 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+	if (!Array.isArray(members)) return false;
+	let team = '';
+	members.forEach(item => {
+		if (typeof item === 'string') {
+			team += item.trim().toUpperCase()[0];
+		}
+	});
+	return team.split('').sort().join('');
 };
