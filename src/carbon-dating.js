f3112caf008db00637c1cd40c85ab902c40c1154
deltapoint <60572499+deltapoint@users.noreply.github.com>
2020-03-22 23:01:56 +0300

    feat: implement 'carbon-dating' task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,11 @@
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15; 
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+	let parsedSampleActivity = parseFloat(sampleActivity);
+	if (typeof sampleActivity === 'string' && !isNaN(parsedSampleActivity) && Number(parsedSampleActivity) < MODERN_ACTIVITY && Number(parsedSampleActivity) > 0) {
+		return Math.ceil(Math.log(MODERN_ACTIVITY / parsedSampleActivity) / (0.693 / HALF_LIFE_PERIOD));
+	} else {
+		return false;
+	}
 };
