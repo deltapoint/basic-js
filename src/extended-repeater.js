f70e5695ef2b751d08b6d296c5bea7b9e2d675ac
deltapoint <60572499+deltapoint@users.noreply.github.com>
2020-03-22 23:04:29 +0300

    feat: implement 'extended-repeater' task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,12 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+	let { repeatTimes, separator, addition, additionRepeatTimes, additionSeparator } = options;
+	separator = separator || '+';
+	additionSeparator = additionSeparator || '|';
+	addition = (addition === false) ? 'false' : addition;
+	addition = (addition === null) ? 'null' : addition;
+	addition = addition || '';
+	
+	let newStr = new Array(additionRepeatTimes).fill(addition, 0)
+	let string = str + newStr.join(additionSeparator);
+	return new Array(repeatTimes).fill(string, 0).join(separator);
 };
-  
